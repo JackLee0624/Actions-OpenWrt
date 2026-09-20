b1df9290ec337b0b8c1e51425528c657532ba046
JackLee0624 <66351576+JackLee0624@users.noreply.github.com>
2026-09-20 10:09:38 +0800

    chore: K2P only-OpenClash config

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# 文件: diy-part1.sh
+# 说明: 在「更新 feeds 之前」执行的定制脚本
+#       适用于 P3TERX/Actions-OpenWrt 模板
+#       作用: 追加第三方软件源（OpenClash 官方源）
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# ==== 追加 OpenClash 官方源码（vernesong/OpenClash）====
+# 之后 workflow 里的 ./scripts/feeds update -a / install -a
+# 会自动把 luci-app-openclash 拉进来，配合 .config 里的
+# CONFIG_PACKAGE_luci-app-openclash=y 即可只编译该插件
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# ==== 可选: 修改默认后台 IP (默认 192.168.1.1) ====
+# 取消下一行注释即可改成 192.168.50.1
+# sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
